84313d2ff4b6fece0cc16fb2b8c90a5539e71f5d
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-09-21 14:50:09 +0530

    Create ques1.c

---
+++ c/Day3/ques1.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main(){
+int week;
+printf("Enter the week no\n");
+scanf("%d",&week);
+if(week==1){
+    printf("Monday\n");
+}
+else if(week==2){
+    printf("Tuesday\n");
+
+}
+else if (week==3){
+    printf("Wednesday\n");
+
+}
+else if(week==4){
+    printf("Thuersday\n");
+
+}
+else if(week==5){
+    printf("friday\n");
+
+}
+else if(week==6){
+    printf("saturday\n");
+
+}
+else if(week==7){
+    printf("Sunday\n");
+
+}
+else{
+    printf("Invalid Input\n");
+
+}
+return 0;
+
+
+
+
+
+
+
+
+}
