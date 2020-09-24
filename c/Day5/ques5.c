8d7b67f50533900b5362b29adb17097e2d6f181b
ANKUR SEHRAWAT <61679890+Ankursehrawat15@users.noreply.github.com>
2020-09-24 13:07:30 +0530

    Create ques5.c

---
+++ c/Day5/ques5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int i,num;
+    printf("Enter the number\n");
+    scanf("%d",&num);
+    for(i=1;i<=num;i++){
+        if(i%2==0){
+            printf("%d\n",i);
+        }
+    }
+
+    
+    return 0;
+}
