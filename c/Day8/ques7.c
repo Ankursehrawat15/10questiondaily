49eb7f4b68206bd78c46134b85e919ccb22783ef
ANKUR SEHRAWAT <ankursehrawat150999@gmail.com>
2020-10-02 23:08:05 +0530

    Update ques7.c

--- c/Day8/ques7.c
+++ c/Day8/ques7.c
@@ -2,9 +2,10 @@
 int test(int a , int b){
     return (a>=100 && a<=200) || (b>=100 && b<=200);
     }
-void main(){
+ int main(){
     int num1,num2;
     printf("Enter the two numbers: ");
     scanf("%d%d", &num1, &num2);
     printf("%d",test(num1, num2));
+     return 0;
 }
